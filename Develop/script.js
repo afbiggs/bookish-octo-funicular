b318e005e78ab84114e0ae798343b1343b738a9b
afbiggs <112245251+afbiggs@users.noreply.github.com>
2022-10-18 23:31:12 -0400

    added completed html file as well on css and javascript which are not complete

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     // listen for save button clicks
     $('.saveBtn').on('click', function () {
         // get nearby values
-        var value =  $(this).siblings
+        var value =  $(this).siblings('.description').val();
         var time = $(this).parent().attr('id')
-
+        console.log("this", time)
         // save in localStorage
         localStorage.setItem(time, value) 
 
